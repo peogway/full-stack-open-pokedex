f63c40826269688a0bc6a51e3bbc4e099cba43b2
peogway <duchungbeo2004@gmail.com>
2025-08-28 00:22:12 +0300

    test for navigating pokemon page

--- e2e-tests/frontpage.cy.js
+++ e2e-tests/frontpage.cy.js
@@ -7,5 +7,12 @@ describe('Pokedex', function () {
 			'Pokémon and Pokémon character names are trademarks of Nintendo.'
 		)
 	})
+
+	it(' pokemon page can be navigated to', function () {
+		cy.visit('http://localhost:5000')
+		cy.contains('ivysaur').click()
+		cy.contains('chlorophyll')
+		cy.contains('overgrow')
+	})
 })
 
