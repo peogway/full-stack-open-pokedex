cfc85065b8482d39d71ea09888b2f5dda69bd993
peogway <duchungbeo2004@gmail.com>
2025-08-28 00:03:15 +0300

    config cypress

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+	e2e: {
+		setupNodeEvents(on, config) {},
+		baseUrl: 'http://localhost:5000',
+		specPattern: 'e2e-tests/**/*.cy.js',
+		supportFile: false,
+	},
+})
+
